c104e6281b3c311659f1c208b0488ac1ab9f722a
Justina <Justina@Justinas-MacBook-Pro.local>
2022-01-03 21:54:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+//**$products = [
+    //[
+    //'name' => 'Siulai', 
+    //'price' => 12.99,
+    //'specia_price' => 10.99,
+    //'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+    //],
+    //[
+    //'name' => 'adata',
+    //'price' => 1.99,
+    //'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
+    //],
+    //[
+    //'name' => 'virbalai',
+    //'price' => 3.99,
+    //'special_price' => 1.99,
+    //'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
+   // ],
+  //* ];
+ //for($y = 0; $y < 10; $y++){
+   // for($x = 0; $x < 10; $x++){
+   // echo '.';
+    //}
+    //echo '<br>';
+   // }
+   // $products = [
+    /*$products = [
+      [
+      'name' => 'Siulai', 
+      'price' => 12.4,
+      'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+      ],
+      [
+      'name' => 'adata',
+      'price' => 1.99,
+      'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
+      ],
+      [
+      'name' => 'virbalai',
+      'price' => 3.99,
+      'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
+      ],
+      ];
+      
+      foreach($products as $product){
+      echo '<img width="60" src="'. $product['img'] .'"/>';
+      echo '<h2>'. $product["name"] . '</h2>';
+      echo '<h3>'. $product["price"] . '</h3>';
+      echo '<hr>';
+      }
+      */
